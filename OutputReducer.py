f8b2695a7093423cbbf1d478ccef5ee3fb9a3d29
pordyna <p.ordyna@hzdr.de>
2021-04-09 18:04:51 +0200

    introduce field-reduce with mpi support
    version that works so far with bp4 -> bp4
    
    add json configs, written field_reduce.py, cleaned up meshes and exclude in Output Reducer
    
    fix parameter input
    
    reformat code, add numba cache=True
    
    mpi multi-node support
    
    add prints from ranks
    
    fixed pipe imports
    
    open series in serial when mpi size 1
    
    fixes
    
    chunk dim = 0
    
    fix chunk dim = 0
    
    remove debug message

---
+++ OutputReducer.py
@@ -0,0 +1,152 @@
+import openpmd_api as api
+from openpmd_api.pipe import FallbackMPICommunicator, Chunk
+import os.path
+import time
+import numpy as np
+from typing import Optional, Iterable, Tuple
+
+try:
+    from mpi4py import MPI
+
+    HAVE_MPI = True
+except ImportError:
+    HAVE_MPI = False
+
+from downscale_kernel import downscale
+
+
+def copy_attributes(source: api.Attributable,
+                    target: api.Attributable) -> None:
+    dtypes = source.attribute_dtypes
+    for attribute in source.attributes:
+        target.set_attribute(attribute, source.get_attribute(attribute), dtypes[attribute])
+
+
+class OutputReducer:
+
+    def __init__(self, source_path: str, output_path: str, div_x: int,
+                 div_y: int, div_z: int,
+                 meshes: Optional[Iterable[str]] = None,
+                 exclude: Optional[Iterable[str]] = None,
+                 wait: bool = False, options_in='{}', options_out='{}'):
+        if meshes is not None and exclude is not None:
+            raise ValueError("meshes and exclude are exclusive optional arguments and can't be set at the same time")
+        if meshes is not None:
+            self.meshes_to_reduce = meshes
+        else:
+            self.meshes_to_reduce = []
+        if exclude is not None:
+            self.meshes_to_exclude = exclude
+        else:
+            self.meshes_to_exclude = []
+        self.axis_scaling = {'x': div_x, 'y': div_y, 'z': div_z}
+        if HAVE_MPI:
+            self.comm = MPI.COMM_WORLD
+            print(f"You are using MPI. Welcome from rank {self.comm.rank} of {self.comm.size}")
+        else:
+            self.comm = FallbackMPICommunicator()
+            print("In serial mode")
+        if self.comm.size == 1:
+            self.output_series = api.Series(output_path, api.Access.create, options_out)
+        else:
+            self.output_series = api.Series(output_path, api.Access.create, self.comm, options_out)
+        if wait:
+            while not os.path.exists(source_path):
+                time.sleep(10)
+        if self.comm.size == 1:
+            self.input_series = api.Series(source_path, api.Access.read_only, options_in)
+        else:
+            self.input_series = api.Series(source_path, api.Access.read_only, self.comm, options_in)
+        self.stored_meshes: dict[str, dict[str, Tuple[np.ndarray, Tuple]]] = {}
+
+    def _to_be_reduced(self, mesh_name: str) -> bool:
+        # no options set:
+        if not self.meshes_to_reduce and not self.meshes_to_exclude:
+            return True
+        # meshes used
+        elif self.meshes_to_reduce:
+            return mesh_name in self.meshes_to_reduce
+        # exclude used
+        else:
+            return mesh_name not in self.meshes_to_exclude
+
+    def _process_mrc_before_close(self, input_mrc: api.Mesh_Record_Component,
+                                  output_mrc: api.Mesh_Record_Component, mesh_dict: dict, mrc_name: str) -> None:
+        copy_attributes(input_mrc, output_mrc)
+        shape = input_mrc.shape
+        offset = [0 for _ in shape]
+        chunk = Chunk(offset, shape)
+        local_chunk = chunk.slice1D(self.comm.rank, self.comm.size, dimension=0)
+        input_data = input_mrc.load_chunk(local_chunk.offset, local_chunk.extent)
+        self.input_series.flush()
+        mesh_dict[mrc_name] = (input_data, shape)
+
+    def _process_mesh_before_close(self, input_mesh: api.Mesh, output_mesh: api.Mesh, mesh_dict: dict) -> None:
+        copy_attributes(input_mesh, output_mesh)
+        # go over record components:
+        for mrc_name in input_mesh:
+            self._process_mrc_before_close(input_mesh[mrc_name], output_mesh[mrc_name], mesh_dict, mrc_name),
+
+    def run(self):
+        copy_attributes(self.input_series, self.output_series)
+        write_iterations = self.output_series.write_iterations()
+        for input_iteration in self.input_series.read_iterations():
+            idx = input_iteration.iteration_index
+            print(f"[rank: {self.comm.rank}]:  Starting to process iteration number {idx}."
+                  f" Starting to read data from source.")
+            # create iteration and copy attributes
+            output_iteration = write_iterations[idx]
+            copy_attributes(input_iteration, output_iteration)
+            # self.output_series.flush()
+
+            # handle meshes
+            input_meshes = input_iteration.meshes
+            output_meshes = output_iteration.meshes
+            copy_attributes(input_meshes, output_meshes)
+            # self.output_series.flush()
+
+            for mesh_name in input_meshes:
+                mesh_dict = self.stored_meshes[mesh_name] = {}
+                self._process_mesh_before_close(input_meshes[mesh_name], output_meshes[mesh_name], mesh_dict)
+            input_iteration.close()
+            print(f"[rank: {self.comm.rank}]: Iteration number {idx} : All iteration data loaded from source. "
+                  f"Now, processing and writing data.")
+            for mesh_name, mesh_dict in self.stored_meshes.items():
+                mesh = output_iteration.meshes[mesh_name]
+                new_grid_spacing = mesh.grid_spacing
+                if self._to_be_reduced(mesh_name):
+                    for dd in range(len(new_grid_spacing)):
+                        new_grid_spacing[dd] *= self.axis_scaling[mesh.axis_labels[dd]]
+                    mesh.set_grid_spacing(new_grid_spacing)
+                for mrc_name, (mrc_data_old, old_global_shape) in self.stored_meshes[mesh_name].items():
+                    if self._to_be_reduced(mesh_name):
+                        new_global_shape = list(old_global_shape)
+                        for dd in range(len(new_global_shape)):
+                            new_global_shape[dd] = new_global_shape[dd] // self.axis_scaling[mesh.axis_labels[dd]]
+                        offset = [0 for _ in new_global_shape]
+                        global_chunk = Chunk(offset, new_global_shape)
+                        local_chunk = global_chunk.slice1D(self.comm.rank, self.comm.size, dimension=0)
+                        for dd, extent in enumerate(local_chunk.extent):
+                            if mrc_data_old.shape[dd] % extent != 0:
+                                raise ValueError(f"[rank: {self.comm.rank}, mesh: {mesh_name}, mrc: {mrc_name}]: "
+                                                 f"Local output shape {local_chunk.extent} is not compatible with the "
+                                                 f"local input shape {mrc_data_old.shape}. {local_chunk.extent[dd]} "
+                                                 f"does not divide {mrc_data_old.shape[dd]}!")
+
+                        mrc_data = np.empty(shape=local_chunk.extent, dtype=mrc_data_old.dtype)
+                        downscale(mrc_data_old, mrc_data)
+                    else:
+                        offset = [0 for _ in old_global_shape]
+                        global_chunk = Chunk(offset, old_global_shape)
+                        local_chunk = global_chunk.slice1D(self.comm.rank, self.comm.size, dimension=0)
+                        mrc_data = mrc_data_old
+                    mrc = mesh[mrc_name]
+                    dataset = api.Dataset(mrc_data.dtype, global_chunk.extent)
+                    mrc.reset_dataset(dataset)
+                    # print(f"[rank: {self.comm.rank}, mesh: {mesh_name}, mrc: {mrc_name}]: "
+                    #       f"mrc_data.shape: {mrc_data.shape}, local_chunk.offset: {local_chunk.offset}"
+                    #       f" local_chunk.extent {local_chunk.extent}")
+                    mrc.store_chunk(mrc_data, local_chunk.offset, local_chunk.extent)
+            output_iteration.close()
+            self.stored_meshes = {}
+            print(f"[rank: {self.comm.rank}]: Finished processing iteration number {idx}.")
---
+++ field_reduce.py
@@ -0,0 +1,92 @@
+import argparse
+import json
+
+from OutputReducer import OutputReducer
+
+# import sys
+# sys.path.append('/trinity/shared/pkg/devel/python/3.6.5/lib/python3.6/site-packages')
+# try:
+#     from mpi4py import MPI
+#     HAVE_MPI = True
+# except ImportError:
+#     HAVE_MPI = False
+
+def main():
+    # Define command line arguments:
+    parser = argparse.ArgumentParser(
+        description="Reads an openPMD series and reduces fields(meshes) resolution by pixel binning. The output is "
+                    "written into another openPMD series. Attributes are preserved as well."
+                    "There is no support for patches/particles, they will be ignored and will not be saved."
+                    "The main use case is to read from an SST stream series and save to a file based series to reduce"
+                    "the amount of data written to disk. Though it should work with other combinations like "
+                    "file -> file or stream -> stream as well."
+    )
+    parser.add_argument("source_path",
+                        help="Path to the .sst file of the input stream, or alternatively to a file based openPMD"
+                             " series.",
+                        type=str)
+    parser.add_argument("output_path",
+                        help="Path to where the new series should be created. Should include sth like /Data_%%T.bp "
+                             "at the end, to specify the backend.",
+                        type=str)
+    parser.add_argument("-x", "--div-x",
+                        help="The number of cells in x directions will be reduced by this value. Has to be an integer."
+                             "Has to divide the number of cells in x direction in the source. (when calling with mpi "
+                             "this has to be true for each chunk, global extend is sliced along the longest side)",
+                        type=int,
+                        default=1)
+    parser.add_argument("-y", "--div-y",
+                        help="The number of cells in y directions will be reduced by this value. Has to be an integer."
+                             "Has to divide the number of cells in y direction in the source. (when calling with mpi "
+                             "this has to be true for each chunk, global extend is sliced along the longest side)",
+                        type=int,
+                        default=1)
+    parser.add_argument("-z", "--div-z",
+                        help="The number of cells in z directions will be reduced by this value. Has to be an integer."
+                             "Has to divide the number of cells in z direction in the source. (when calling with mpi "
+                             "this has to be true for each chunk, global extend is sliced along the longest side)",
+                        type=int,
+                        default=1)
+    parser.add_argument('-m', '--meshes', nargs='+', type=str, default=[],
+                        help="Meshes should have the reduction applied. Can't be used together with --exclude. "
+                             "If not set all, but for ones listed with --exclude, meshes will be processed. "
+                             "Note, other meshes will be still copied in their original resolution.")
+    parser.add_argument('-e', '--exclude', nargs='+', type=str, default=[],
+                        help="A list of meshes to exclude from reduction. Can't be used together with --meshes. "
+                             "Note, these meshes will be still copied in their original resolution.")
+    parser.add_argument("-w", "--wait", action='store_true',
+                        help="When set the script will wait until the source path points to an existing file. "
+                             "Use this when the source path points to an .sst file and the writer may not have yet"
+                             " created it when the script is trying to open the series.")
+    parser.add_argument("-s", "--source-config-path",
+                        help="Path to an .json file that specifies the backend specific configuration for the "
+                             "source openPMD series.", default='{}',
+                        type=str)
+    parser.add_argument("-o", "--output-config-path",
+                        help="Path to an .json file that specifies the backend specific configuration for the "
+                             "output openPMD series.", default='{}',
+                        type=str)
+    args = parser.parse_args()
+
+    with open(args.source_config_path, 'r') as json_data:
+        options_input_string = json.dumps(json.load(json_data))
+    with open(args.output_config_path, 'r') as json_data:
+        options_output_string = json.dumps(json.load(json_data))
+    if args.meshes:
+        meshes = args.meshes
+    else:
+        meshes = None
+    if args.exclude:
+        exclude = args.exclude
+    else:
+        exclude = None
+    reducer = OutputReducer(args.source_path, args.output_path, args.div_x, args.div_y, args.div_z, meshes, exclude,
+                            args.wait,
+                            options_input_string, options_output_string)
+    print("Successfully initialized. Input and output series are open. Running now!")
+    reducer.run()
+    del reducer
+
+
+if __name__ == "__main__":
+    main()
