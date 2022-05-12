fix averaging and add unit tests

OutputReducer.py
@@ -1,6 +1,5 @@
-
 import openpmd_api as api
-from openpmd_api.pipe import FallbackMPICommunicator, Chunk
+from openpmd_api.pipe.__main__ import FallbackMPICommunicator, Chunk
 import os.path
 import time
 import numpy as np
@@ -8,6 +7,7 @@ from typing import Optional, Iterable, Tuple
 
 try:
     from mpi4py import MPI
+
     HAVE_MPI = True
 except ImportError:
     HAVE_MPI = False
@@ -65,10 +65,11 @@ class OutputReducer:
         print("opened input series")
         self.stored_meshes: dict[str, dict[str, Tuple[np.ndarray, Tuple]]] = {}
         self.last_iteration = last_iteration
-    
+
     def finalize(self):
         del self.output_series
         del self.input_series
+
     def _to_be_reduced(self, mesh_name: str) -> bool:
         # no options set:
         if not self.meshes_to_reduce and not self.meshes_to_exclude:
@@ -88,7 +89,7 @@ class OutputReducer:
         chunk = Chunk(offset, shape)
         local_chunk = chunk.slice1D(self.comm.rank, self.comm.size, dimension=0)
         input_data = input_mrc.load_chunk(local_chunk.offset, local_chunk.extent)
-        #self.input_series.flush()
+        # self.input_series.flush()
         mesh_dict[mrc_name] = (input_data, shape)
 
     def _process_mesh_before_close(self, input_mesh: api.Mesh, output_mesh: api.Mesh, mesh_dict: dict) -> None:
@@ -119,7 +120,7 @@ class OutputReducer:
                 mesh_dict = self.stored_meshes[mesh_name] = {}
                 self._process_mesh_before_close(input_meshes[mesh_name], output_meshes[mesh_name], mesh_dict)
                 # input_iteration.close()
-                #print(f"[rank: {self.comm.rank}]: Iteration number {idx} : All iteration data loaded from source. "
+                # print(f"[rank: {self.comm.rank}]: Iteration number {idx} : All iteration data loaded from source. "
                 #      f"Now, processing and writing data.")
                 self.input_series.flush()
                 mesh = output_iteration.meshes[mesh_name]
@@ -157,10 +158,9 @@ class OutputReducer:
                     #       f"mrc_data.shape: {mrc_data.shape}, local_chunk.offset: {local_chunk.offset}"
                     #       f" local_chunk.extent {local_chunk.extent}")
                     mrc.store_chunk(mrc_data, local_chunk.offset, local_chunk.extent)
-                #self.output_series.flush()
                 self.stored_meshes = {}
             input_iteration.close()
             output_iteration.close()
             print(f"[rank: {self.comm.rank}]: Finished processing iteration number {idx}.")
-            if (self.last_iteration <= idx and self.last_iteration >0):
-               break
+            if idx >= self.last_iteration > 0:
+                break

downscale_kernel.py
@@ -3,38 +3,53 @@ from numba import njit, prange, generated_jit
 
 
 @njit(parallel=True, inline='always', cache=True)
-def _downscale_1d(input_arr, output_arr):
+def _reduce_1d(input_arr, output_arr):
     bin_length = input_arr.shape[0] // output_arr.shape[0]
     for ii in prange(output_arr.shape[0]):
-        output_arr[ii] = np.mean(input_arr[bin_length * ii: bin_length * (ii + 1)])
+        output_arr[ii] = np.sum(input_arr[bin_length * ii: bin_length * (ii + 1)])
+
+
+@njit(parallel=True, inline='always', cache=True)
+def _downscale_1d(input_arr, output_arr):
+    bin_length = input_arr.shape[0] // output_arr.shape[0]
+    _reduce_1d(input_arr, output_arr)
+    output_arr /= bin_length
 
 
 @njit(parallel=True, inline='always', cache=True)
 def _reduce_2d_1(input_arr, output_arr):
     for ii in prange(output_arr.shape[0]):
-        _downscale_1d(input_arr[ii, :], output_arr[ii, :])
+        _reduce_1d(input_arr[ii, :], output_arr[ii, :])
 
 
 @njit(parallel=True, inline='always', cache=True)
 def _reduce_2d_0(input_arr, output_arr):
     for ii in prange(output_arr.shape[1]):
-        _downscale_1d(input_arr[:, ii], output_arr[:, ii])
+        _reduce_1d(input_arr[:, ii], output_arr[:, ii])
 
 
-@njit(parellel=True, inline='always', cache=True)
-def _downscale_2d(input_arr, output_arr):
+@njit(parallel=True, inline='always', cache=True)
+def _reduce_2d(input_arr, output_arr):
     tmp_array = np.empty((input_arr.shape[0], output_arr.shape[1]))
     _reduce_2d_1(input_arr, tmp_array)
     _reduce_2d_0(tmp_array, output_arr)
 
+@njit(parallel=True, inline='always', cache=True)
+def _downscale_2d(input_arr, output_arr):
+    _reduce_2d(input_arr, output_arr)
+    cells_in_bin = input_arr.shape[0] / output_arr.shape[0] * input_arr.shape[1] / output_arr.shape[1]
+    output_arr[:, :] = output_arr / cells_in_bin
 
-@njit(parellel=True, inline='always', cache=True)
+@njit(parallel=True, inline='always', cache=True)
 def _downscale_3d(input_arr, output_arr):
     tmp_array = np.empty((input_arr.shape[0], output_arr.shape[1], output_arr.shape[2]))
     for ii in prange(input_arr.shape[0]):
-        _downscale_2d(input_arr[ii, :, :], tmp_array[ii, :, :])
+        _reduce_2d(input_arr[ii, :, :], tmp_array[ii, :, :])
     for ii in prange(output_arr.shape[2]):
         _reduce_2d_0(tmp_array[:, :, ii], output_arr[:, :, ii])
+    cells_in_bin = (input_arr.shape[0] / output_arr.shape[0] * input_arr.shape[1] / output_arr.shape[1]
+                    * input_arr.shape[2] / output_arr.shape[2])
+    output_arr[:, :] = output_arr / cells_in_bin
 
 
 @generated_jit(nopython=True, parallel=True, cache=True)